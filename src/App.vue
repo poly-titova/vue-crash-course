9cd42c1f42db67b300b541cc7e38149ec4fdde35
Poly Titova <poly_titova@mail.ru>
2021-01-02 11:29:38 +0300

    Добавили значения из стороннего API

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,21 @@
 </template>
 
 <script>
-import TodoList from "@/components/TodoList";
+import TodoList from "@/components/TodoList"
 import AddTodo from "@/components/AddTodo"
 export default {
   name: "app",
   data() {
     return {
-      todos: [
-        { id: 1, title: "Купить хлеб", complited: false },
-        { id: 2, title: "Купить масло", complited: false },
-        { id: 3, title: "Купить молоко", complited: false },
-      ],
+      todos: []
     };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=3")
+      .then((response) => response.json())
+      .then((json) => {
+        this.todos = json
+      });
   },
   methods: {
     removeTodo(id) {
